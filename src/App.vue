e3e31ab7af96d6aeed0315b0b3036f5b2a6ecf6d
Aleksey Belchenkov <u608110@gmail.com>
2019-06-23 07:21:34 +0300

    Adding a carousel

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <comp-header></comp-header>
     <div>
-      content
+      <comp-featured />
     </div>
     <comp-footer></comp-footer>
   </div>
@@ -11,12 +11,14 @@
 <script>
 import compHeader from './components/Header-footer/Header';
 import compFooter from './components/Header-footer/Footer';
+import compFeatured from './components/Featured/index';
 
 export default {
   name: 'app',
   components: {
     compHeader,
-    compFooter
+    compFooter,
+    compFeatured
   }
 }
 </script>
