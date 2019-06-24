efce5cd43d8cb15fc494aa64a3ae96bbebe886e6
Aleksey Belchenkov <u608110@gmail.com>
2019-06-24 06:48:01 +0300

    Finishing Menu

--- src/components/Menu/index.vue
+++ src/components/Menu/index.vue
@@ -13,6 +13,29 @@
                 {{ button.name }}
             </md-button>
         </div>
+        <div class="products_items">
+            <md-card
+                v-for="(product, index) in products"
+                :key="index"
+                v-show="product.type === activeProduct"
+            >
+                <md-card-media md-ratio="16:9">
+                    <img
+                        :src="require(`../../assets/images/products/${product.img}`)"
+                        alt="product.name"
+                    >
+                </md-card-media>
+                <md-card-header>
+                    <h2 class="md-title">{{ product.name }}</h2>
+                    <div class="md-subhead">
+                        <span>$ {{ product.price }}</span>
+                    </div>
+                </md-card-header>
+                <md-card-content>
+                    {{ product.desc }}
+                </md-card-content>
+            </md-card>
+        </div>
     </div>
 </template>
 
@@ -27,12 +50,20 @@
                     { name: 'salad', active: false},
                     { name: 'dessert', active: false},
                 ],
+                activeProduct: 'pizza',
                 products: []
             }
         },
         methods: {
             changeMenu(value) {
-                this.buttons.forEach(item => item.name == value ? item.active = true : item.active = false);
+                this.buttons.forEach(item => {
+                    if (item.name == value) {
+                        item.active = true;
+                        this.activeProduct = value;
+                    } else {
+                        item.active = false;
+                    }
+                });
             }
         },
         created() {
@@ -56,5 +87,13 @@
 </script>
 
 <style scoped>
-
+    .md-card {
+        width: 32%;
+        margin-bottom: 20px;
+    }
+    .products_items {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import VueResource from 'vue-resource';
 
 import App from './App.vue'
 
-import { MdButton, MdDrawer } from 'vue-material/dist/components';
+import { MdButton, MdDrawer, MdCard } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 
 Vue.use(VueResource);
 Vue.use(MdButton);
 Vue.use(MdDrawer);
+Vue.use(MdCard);
 
 Vue.http.options.root = 'https://domnoo-8a782.firebaseio.com/';
 
