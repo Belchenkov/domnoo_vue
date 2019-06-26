097e981bab79c9005f5523905192c6e4d5ce27d1
Aleksey Belchenkov <u608110@gmail.com>
2019-06-26 06:09:57 +0300

    Scroll reveal

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource';
+import VueScrollReveal from 'vue-scroll-reveal';
 
 import App from './App.vue'
 
@@ -7,6 +8,11 @@ import { MdButton, MdDrawer, MdCard } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 
 Vue.use(VueResource);
+Vue.use(VueScrollReveal, {
+  duration: 800,
+  scale: 1,
+  distance: '50px'
+});
 Vue.use(MdButton);
 Vue.use(MdDrawer);
 Vue.use(MdCard);
